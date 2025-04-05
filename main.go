7f1071342bb11473060b8b91722b8ebf1dcfa6e5
darthxd <darthp87@gmail.com>
2025-04-05 14:36:01 -0300

    Configured the database

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"log"
+	"os"
 	"text/template"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"github.com/darthxd/tcc-app/handler"
+	"github.com/darthxd/tcc-app/models"
 )
 
 func main() {
+	// Set up basic server settings 
 	e := echo.New()
 	t := &handler.Template{Templates: template.Must(template.ParseGlob("public/views/*.html"))}
 	e.Renderer = t
 	e.Static("/assets", "public/static")
 
+	// Load .env and start the database
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	connStr := os.Getenv("DATABASE_URL")
+	port := os.Getenv("PORT")
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to the database")
+	}
+	db.AutoMigrate(&models.Student{})
+
+	// Set up the main routes
 	student := e.Group("/aluno")
 	{
 		student.GET("/", handler.StudentRender)
@@ -35,7 +54,8 @@ func main() {
 		auth.GET("", handler.AuthRender)
 	}
 
-	if err := e.Start(":8080"); err != nil {
+	// Run the server
+	if err := e.Start(port); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ models/student_model.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Student struct {
+	ID        uint
+	Name      string
+	RM        string
+	Phone     string
+	Birthdate time.Time
+	Course    string
+	Grade     string
+	Email     string
+	Password  string
+	Photo     *string
+	Biometry  *string
+	InSchool  bool
+}
