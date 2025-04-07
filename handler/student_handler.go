0a60119eb62e0d18aff719afc8c7298d0d27d9cd
darthxd <darthp87@gmail.com>
2025-04-07 19:52:02 -0300

    .

--- handler/student_handler.go
+++ handler/student_handler.go
@@ -8,12 +8,14 @@ import (
 
 func StudentInfo(c echo.Context) error {
 	return c.Render(http.StatusOK,"student_home", echo.Map{
+		"title":"Informações",
 		"active":"info",
 	})
 }
 
 func StudentMail(c echo.Context) error {
 	return c.Render(http.StatusOK, "student_mail", echo.Map{
+		"title":"E-mail",
 		"active":"email",
 	})
 }
